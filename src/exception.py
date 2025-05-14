0d3ab2d1aa7c8f2621b05231acf79eea04d8dc1d
Grant654 <sarfogrant567@gmail.com>
2025-05-14 11:19:15 +0000

    Logging and Exception

--- src/exception.py
+++ src/exception.py
@@ -1,6 +1,7 @@
 import sys
 import traceback
 import logging
+from src.logger import logging
 
 def error_message_detail(error, error_detail: sys):
     _, _, exc_tb = error_detail.exc_info()
@@ -19,4 +20,4 @@ class customException(Exception):
     def __str__(self):
         return self.error_message
     
-    
+
