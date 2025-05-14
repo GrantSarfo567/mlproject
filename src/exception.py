3dd244f771a059063fea3691193ac83bf969e6d8
Grant654 <sarfogrant567@gmail.com>
2025-05-14 11:06:17 +0000

    Logging and Exception

--- src/exception.py
+++ src/exception.py
@@ -5,18 +5,18 @@ import logging
 def error_message_detail(error, error_detail: sys):
     _, _, exc_tb = error_detail.exc_info()
     file_name = exc_tb.tb_frame.f_code.co_filename
-    error_message = "Error occurred in Python script [{0}] line number [{1}] error message: [{2}]".format(
+    error_message = "Error occurred in python script name [{0}] line number [{1}] error message [{2}]".format(
         file_name, exc_tb.tb_lineno, str(error)
-    )
-    return error_message
+    )  # Fixed missing closing parenthesis
 
-class CustomException(Exception):
-    def __init__(self, error, error_detail: sys):
-        super().__init__(str(error))  # Properly initialize base Exception
-        self.error_message = error_message_detail(error, error_detail=error_detail)
+    return error_message  # Moved return statement here
+
+class customException(Exception):
+    def __init__(self, error_message, error_detail: sys):
+        super().__init__(error_message)
+        self.error_message = error_message_detail(error_message, error_detail)
 
     def __str__(self):
         return self.error_message
-
-
+    
     
